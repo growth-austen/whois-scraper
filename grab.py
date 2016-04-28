230d3481758a529cf247c3f1c0dfe13086fe374a
austen-lendup <austen.allred@lendup.com>
2016-04-28 16:34:39 -0700

    initial commit

---
+++ grab.py
@@ -0,0 +1,23 @@
+import whois
+import json
+import io
+
+#Take the URL file and turn it into an array
+f = open('urls.txt')
+urls = f.readlines()
+
+f = open('urlinfo.txt', 'a')
+
+# iterate through the URLs
+for url in urls:
+	print "Grabbing %s" %url
+	whois_data = whois.whois(url)
+	f.write(url)
+
+	with io.open('urlinfo.txt', 'a', encoding='utf-8') as f:
+		
+		f.write(unicode(whois_data))
+		
+	f = open('urlinfo.txt', 'a')
+	f.write("\n \n")
+	f.close
